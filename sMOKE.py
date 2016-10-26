006f187ca751b2119d02c835187ba8d25c0e92dc
nikolajreiser <nikolaj.reiser@gmail.com>
2016-10-26 17:10:49 -0500

    Added hc and mrem capabilities

--- sMOKE.py
+++ sMOKE.py
@@ -9,6 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.ndimage import gaussian_filter
 from scipy.signal import argrelextrema
+from matplotlib.widgets import CheckButtons
 
 
 def getFiles(path, data):
@@ -29,15 +30,48 @@ def getData(path):
 def getxy(path):
     return int(path[len(path)-18]), int(path[len(path)-14])
     
-data_path = "/Users/nikolaj/Desktop/rzchowski/161016/1"
+def Hc_of(x, y, ks=2):
+    # Setup indices
+    gt0idx = x >= 0
+    lt0idx = x < 0
+    ymgt0idx = np.argmin(np.abs(y[gt0idx]))
+    ymlt0idx = np.argmin(np.abs(y[lt0idx]))
+    # Compute Hc
+    Hc_gt0 = x[gt0idx][ymgt0idx - ks:ymgt0idx + ks].mean()
+    Hc_lt0 = x[lt0idx][ymlt0idx - ks:ymlt0idx + ks].mean()
+    Hc_avg = (abs(Hc_gt0) + abs(Hc_lt0))/2.
+    return Hc_avg
+    
+def Mrem_of(x, y, ks=3):
+    # Setup indices
+    N = len(x)+1
+    inds = np.arange(N).reshape(4, N//4)
+    yq03 = y[inds[[0, 3]]-1].reshape(N//2) # yq03 = y quarters 0 and 3
+    xq03 = x[inds[[0, 3]]-1].reshape(N//2)
+    yq12 = y[inds[[1, 2]]-1].reshape(N//2)
+    xq12 = x[inds[[1, 2]]-1].reshape(N//2)
+    xmq03i = np.argmin(np.abs(xq03)) # xmq03i = indsof x min quarters 0 and 3
+    xmq12i = np.argmin(np.abs(xq12))
+    # Average over the kernel size
+    yq03avg = abs(np.mean(yq03[xmq03i-ks:xmq03i+ks]))
+    yq12avg = abs(np.mean(yq12[xmq12i-ks:xmq12i+ks]))
+    mrem = (yq03avg + yq12avg)/2.
+    return mrem
+    
+data_path = "/Users/nikolaj/Desktop/rzchowski/161016/0"
 #data = getData(data_path)
 data = glob.glob(data_path+"/*averaged.txt")
+
+
 
 file = open("out.txt", "w") 
 file.write("x\ty\t\tleft_sat(B)\tleft_sat(V)\tright_sat(B)\tright_sat(V)\tleft_slope\tright_slope\tarea\n")
 
 mx, my = getxy(data[len(data)-1])
 f, axarr = plt.subplots(mx+1, my+1)
+
+mrems = np.zeros((mx+1, my+1))
+hcs = np.zeros((mx+1, my+1))
 
 for line in axarr:
     for curr in line:
@@ -62,8 +96,8 @@ for q in range(len(data)):
     """clean/process data """
     
     smoothed = [[],[]]
-    smoothed[0] = gaussian_filter(loop[0], 20)
-    smoothed[1] = gaussian_filter(loop[1], 20)
+    B = smoothed[0] = gaussian_filter(loop[0], 20)
+    V = smoothed[1] = gaussian_filter(loop[1], 20)
     
     dB = smoothed[0]
     dV = np.gradient(smoothed[1])
@@ -78,8 +112,8 @@ for q in range(len(data)):
     tempmax = tempmax[0]
     tempmin = argrelextrema(dV3, np.less)
     tempmin = tempmin[0]
-    high = max(dV3)/2
-    low = min(dV3)/2
+    high = np.max(dV3)/2
+    low = np.min(dV3)/2
     
     upper = high - np.abs(high-low)/4
     lower = low + np.abs(high-low)/4
@@ -124,6 +158,11 @@ for q in range(len(data)):
     bottom_area2=np.trapz(smoothed[1][left:len(smoothed[0])+1],smoothed[0][left:len(smoothed[0])+1])
     area=top_area-(bottom_area1+bottom_area2)
     
+    
+    """find mrem and hc"""
+    mrem = Mrem_of(B,V)
+    hc = Hc_of(B,V)
+    
     """write to output file"""
     
     x,y = getxy(data[q])
@@ -133,18 +172,21 @@ for q in range(len(data)):
     b = smoothed[0]
     v = smoothed[1]
     
-    curr = axarr[int(x), int(y)]
 
     out = x+"\t"+y+"\t"+str(b[satleft])+"\t"+str(v[satleft])+"\t"
     out += str(b[satright])+"\t"+str(v[satright])+"\t"
     out += str(lslope)+ "\t" + str(rslope)+"\t" + str(area)+"\n"
     file.write(out)
+    
+    """plot data"""   
+    curr = axarr[int(x), int(y)]
     curr.set_title(x+","+y, fontsize = 10)
-    curr.plot(b,v, 'k')
+    graph_data = curr.plot(b,v, 'g', alpha = .7)
     curr.plot(b[satright],v[satright],'ro', alpha = .5)
     curr.plot(b[satleft],v[satleft],'bo', alpha = .5)
-    #curr.plot(b[lintercept],v[lintercept], 'b*')
-    #curr.plot(b[rintercept],v[rintercept], 'r*')
+    
+    mrems[int(x)][int(y)] = mrem
+    hcs[int(x)][int(y)] = hc
     
     ts = ts/1000000
     
@@ -156,5 +198,41 @@ for q in range(len(data)):
     
     curr.plot(ltanx, ltany, 'b', linewidth = 2)
     curr.plot(rtanx, rtany, 'r', linewidth = 2)
+    
+"""plot hc and mrem"""
+
+def switch(label):
+    vals = []
+
+    if(label == 'hc'):
+        vals = mrems
+        check.labels[0].set_text('mrem')
+    if(label == 'mrem'):
+        vals = hcs
+        check.labels[0].set_text('hc')
+    for y,line in enumerate(axarr):
+        for x,curr in enumerate(line):
+                curr.set_axis_bgcolor(str(vals[x][y]/np.max(vals)))
+
+    legend_string="hc/mrem values \n"
+    for y, line in enumerate(vals): 
+        for x, val in enumerate(line):
+            legend_string += (str(x)+","+str(y)+":"+str(val)[0:4]+"\n")
+    if(label == 'show\nvalues'):
+        f.text(0, 0, 'hi', visible = False)
+        label = 'hide\nvalues'
+    if(label == 'hide\nvalues'):
+        f.text(.91, 0, legend_string)
+        label = 'show\nvalues'
+    plt.draw()
+
+#switch('hc')
+#switch('show\nvalues')
+rax = plt.axes([0.01, 0.4, 0.095, 0.15])
+check = CheckButtons(rax, ('hc', 'show\nvalues'),  (True, False))
+check.on_clicked(switch)
+
+        
+        
     
 file.close()
